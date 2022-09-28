ea369478944078b16e30a444310c63690fb03334
jbdura <bidurajohnson@gmail.com>
2022-09-28 10:09:17 +0300

    Done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,9 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  # added rescue_from
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+  validates :name, presence: true, uniqueness: true
 
   # GET /birds
   def index
@@ -7,11 +11,32 @@ class BirdsController < ApplicationController
     render json: birds
   end
 
+  ####################################################################################################
+
   # POST /birds
+  # def create
+  #   bird = Bird.create(bird_params)
+  #   if bird.valid?
+  #     render json: bird, status: :created
+  #   else
+  #     render :json: { errors: bird.errors }, status: :unprocessable_entity
+  #   end
+  # end
+
+  # def create
+  #   bird = Bird.create!(bird_params)
+  #   render json: bird, status: :created
+  # rescue ActiveRecord::RecordInvalid => invalid
+  #   render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  # end
+
   def create
-    bird = Bird.create(bird_params)
+    # create! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
+    bird = Bird.create!(bird_params)
     render json: bird, status: :created
   end
+
+  ####################################################################################################
 
   # GET /birds/:id
   def show
@@ -20,11 +45,30 @@ class BirdsController < ApplicationController
   end
 
   # PATCH /birds/:id
+  # def update
+  #   bird = find_bird
+  #   bird.update(bird_params)
+  #   render json: bird
+  # end
+
+  ######################################################################################################
+
+  # def update
+  #   bird = find_bird
+  #   bird.update!(bird_params)
+  #   render json: bird
+  # rescue json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  # end
+
   def update
     bird = find_bird
-    bird.update(bird_params)
+    # update! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
+    bird.update!(bird_params)
     render json: bird
   end
+
+  ####################################################################################################
+
 
   # DELETE /birds/:id
   def destroy
@@ -33,7 +77,16 @@ class BirdsController < ApplicationController
     head :no_content
   end
 
+
+  ####################################################################################################
+
+
   private
+
+  def render_unprocessable_entity_response(invalid)
+    # render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
 
   def find_bird
     Bird.find(params[:id])
